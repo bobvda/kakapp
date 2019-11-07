261c99d50f32f3a885cfcc1519eeb8328a30ffb0
Bob Van den Audenaerde <s097008@ap.be>
2019-11-07 09:59:04 +0100

    test commit

--- public/js/script.js
+++ public/js/script.js
@@ -20,7 +20,7 @@ var basicmap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png
 	maxZoom: 17,
 	attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 });
-
+/**/
 /*
  * bikemap initialiseren
  */
