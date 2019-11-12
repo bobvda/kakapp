040b5c7475d96c498e54e04357cdf713e06e8edb
Thijs <thijs.vinckx@student.ap.be>
2019-11-12 12:42:06 +0100

    Toilet icon added

--- public/js/script.js
+++ public/js/script.js
@@ -7,8 +7,12 @@ var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
 /*
  *initiaisatie
  */
+//openen op vaste locatie
+//var mymap = L.map("mapid").setView([51.219496, 4.401636], 18); //coordinates + zoom
 
+//openen op huidige locatie
 var mymap = L.map("mapid").setView([51.219496, 4.401636], 18); //coordinates + zoom
+
 // activering
 var basicmap = L.tileLayer('https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey={apikey}', {
 	attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
@@ -22,22 +26,23 @@ var basicmap = L.tileLayer('https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}
 
 var LeafIcon = L.Icon.extend({
     options: {
-        iconSize:     [40,40],
-        iconAnchor:   [20,20],
-        popupAnchor:  [0,-20],
+        iconSize:     [50,50],
+        iconAnchor:   [25,25],
+        popupAnchor:  [0,-25],
     }
 });
 
-var greenIcon = new LeafIcon({iconUrl: 'tentGreen.png'}),
-    blueIcon = new LeafIcon({iconUrl: 'tentBlue.png'}),
-    orangeIcon = new LeafIcon({iconUrl: 'tentOrange.png'});
+// Afbeelding linken aan custom item
 
+var toiletIcon = new LeafIcon({iconUrl: '/./leaflet/toiletIcon.png'});
 
+// WC iconen ter voorbeeld
 
-// HANGOUTS
-var hangOutOne = L.marker([51.2016242,4.458387799999969], {icon: greenIcon}).addTo(mymap).bindPopup("Bij Flor en Kaat."); //Flor en Kaat in Deurne
-var hangOutTwo = L.marker([51.2171919,4.421446100000026], {icon: blueIcon}).addTo(mymap).bindPopup("Op de trein."); //Centraal Station
-var hangOutThree = L.marker([51.22789390000001,4.405467499999986], {icon: orangeIcon}).addTo(mymap).bindPopup("Story Urban Deli Shop");
-var marker = L.marker(latlng).addTo(mymap).bindPopup("U bevindt zich hier");
+var hangOutOne = L.marker([51.2016242,4.458387799999969], {icon: toiletIcon}).addTo(mymap).bindPopup("Aanklikbare link");
+var hangOutTwo = L.marker([51.2171919,4.421446100000026], {icon: toiletIcon}).addTo(mymap).bindPopup("Aanklikbare link");
+var hangOutThree = L.marker([51.22789390000001,4.405467499999986], {icon: toiletIcon}).addTo(mymap).bindPopup("Aanklikbare link");
+var marker = L.marker(latlng).addTo(mymap).bindPopup("Huidige locatie");
+
+// Hier moet een functie komen die locaties uit de databank haalt en daar icoontjes plaatst.
 
 });
