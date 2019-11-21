27089c80683aeaf372afe0a60bc72bf8cc36776c
Bob Van den Audenaerde <s097008@ap.be>
2019-11-21 09:23:31 +0100

    Header fix

--- public/js/script.js
+++ public/js/script.js
@@ -13,6 +13,9 @@ var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
 //openen op huidige locatie
 var mymap = L.map("mapid").setView([51.219496, 4.401636], 18); //coordinates + zoom
 
+
+
+
 // activering
 var basicmap = L.tileLayer('https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey={apikey}', {
 	attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
